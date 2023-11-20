f546b867845edd7a03ae1536585d6f92a9dd2342
PetrVelikiy <PetrVelikiy@gmail.com>
2023-11-20 18:47:56 +0300

    "Дженерик для дом.задания"

---
+++ src/Model/Service/TreeInterface.java
@@ -0,0 +1,25 @@
+package Model.Service;
+
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TreeInterface<E> extends Serializable {
+    boolean addParents(E parents);
+    boolean addChild(E child);
+    List<E> getChildren();
+    String getName();
+    LocalDate getBirthdate();
+    LocalDate getDeathdate();
+    void setChildren(List<E> children);
+    void setMother(E mother);
+    void setFather(E father);
+    List<E> getParents();
+    int getAge();
+    void setMarriage(E marriage);
+    E getMarriage();
+    void setId(long l);
+    long getId();
+}
